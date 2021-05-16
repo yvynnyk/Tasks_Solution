b4b4167762637cec3e80761be04a8dbf61815c6e
Iurii <juravyn@gmail.com>
2021-05-16 22:32:29 +0300

    Add project files.

---
+++ Task1_Pyramid/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace SimpleApplication
+{
+    class Pyramid
+    {
+        static void Main(string[] args)
+        {
+            int rows = 7;
+            PrintPyramid(rows);
+        }
+        static void PrintPyramid(int totalRows)
+        {
+            for (int i = 0; i < totalRows; i++)
+            {
+                PrintSpaces(i, totalRows);
+                PrintAsterisk(i);
+                GoToNextRow();
+            }
+        }
+        static void PrintSpaces(int rowNumber, int totalRows)
+        {
+            for (int i = (totalRows * 2) - 1 - (rowNumber * 2); i > 0; i--)
+            {
+                PrintOneSpace();
+            }
+        }
+        static void PrintAsterisk(int rowNumber)
+        {
+            for (int i = 0; i < (rowNumber + 1) * 2 - 1; i++)
+            {
+                Console.Write("*");
+                PrintOneSpace();
+            }
+        }
+        static void PrintOneSpace()
+        {
+            Console.Write(" ");
+        }
+        static void GoToNextRow()
+        {
+            Console.WriteLine();
+        }
+    }
+}
+
---
+++ Task2_VaribleDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace VariablesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            byte languageValue;
+            do
+            {
+                Console.Write("About which language do you want to know information? 1 - English, 2 - Spanish, 3 - Ukrainian ");
+                line = Console.ReadLine();
+            } while (CheckLine(byte.TryParse(line, out languageValue), languageValue));
+
+            ShowInfo(languageValue);
+        }
+        public enum Language : byte
+        {
+            English = 1,
+            Spanish = 2,
+            Ukrainian = 3
+        }
+        static bool CheckLine(bool isParsed, byte languageValue)
+        {
+            if (isParsed && languageValue >= 1 && languageValue <= 3)
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Please follow the instructions and try again");
+                return true;
+            }
+        }
+        static void ShowInfo(byte languageValue)
+        {
+            Console.WriteLine($"With 2,500 to 3,000 words, you can understand 90% of everyday {(Language)languageValue} conversations,");
+            Console.WriteLine($"{(Language)languageValue} newspaper and magazine articles, and {(Language)languageValue} used in the workplace.");
+            Console.WriteLine($"So it is essential to learn the right {(Language)languageValue} vocabulary words.");
+        }
+    }
+}
